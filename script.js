0aaf0c40ad4a231f007fd33b726367331a5263e3
thevijeeth@gmail.com <thevijeeth@gmail.com>
2023-10-27 00:28:52 +0530

    add feature-tabbed component

--- script.js
+++ script.js
@@ -8,50 +8,77 @@ function navToggle() {
   menu.classList.toggle("flex");
   menu.classList.toggle("hidden");
 }
-const src = [
-  "ressources/roman.jpg",
-  "ressources/danish.jpg",
-  "ressources/mideval.jpg",
-  "ressources/eighteen.jpg",
-  "ressources/18thcentuerysvg.png",
-  "ressources/13879.jpg",
-  "ressources/6961780.jpg",
-];
+// const src = [
+//   "ressources/roman.jpg",
+//   "ressources/danish.jpg",
+//   "ressources/mideval.jpg",
+//   "ressources/eighteen.jpg",
+//   "ressources/18thcentuerysvg.png",
+//   "ressources/13879.jpg",
+//   "ressources/6961780.jpg",
+// ];
 
-// const dates = ["-3300 | 476", "476 | 1492", "1492 | 1792", "1792 | AJD"];
+// // const dates = ["-3300 | 476", "476 | 1492", "1492 | 1792", "1792 | AJD"];
 
-ScrollTrigger.create({
-  trigger: ".sections",
-  pin: ".tooltip",
-  // Trigger Scroller
-  start: "top 40%",
-  end: "bottom 55%",
-  // markers: true
-});
+// ScrollTrigger.create({
+//   trigger: ".sections",
+//   pin: ".tooltip",
+//   // Trigger Scroller
+//   start: "top 40%",
+//   end: "bottom 55%",
+//   // markers: true
+// });
+
+// const sections = document.querySelectorAll(".section");
+
+// sections.forEach((section, index) => {
+//   // console.log(index);
+//   ScrollTrigger.create({
+//     trigger: section,
+//     start: "top center",
+//     end: "bottom center",
+//     onEnter: () => {
+//       gsap.set(".tooltip-img", {
+//         attr: { src: src[index] },
+//       });
+//       // gsap.set(".tooltip p", {
+//       //   innerText: dates[index],
+//       // });
+//     },
+//     onEnterBack: () => {
+//       gsap.set(".tooltip-img", {
+//         attr: { src: src[index] },
+//       });
+//       // gsap.set(".tooltip p", {
+//       //   innerText: dates[index],
+//       // });
+//     },
+//   });
+// });
 
 const sections = document.querySelectorAll(".section");
+let currentSection = 0;
 
-sections.forEach((section, index) => {
-  // console.log(index);
-  ScrollTrigger.create({
-    trigger: section,
-    start: "top center",
-    end: "bottom center",
-    onEnter: () => {
-      gsap.set(".tooltip-img", {
-        attr: { src: src[index] },
-      });
-      // gsap.set(".tooltip p", {
-      //   innerText: dates[index],
-      // });
-    },
-    onEnterBack: () => {
-      gsap.set(".tooltip-img", {
-        attr: { src: src[index] },
-      });
-      // gsap.set(".tooltip p", {
-      //   innerText: dates[index],
-      // });
-    },
-  });
-});
+function showNextSection(button) {
+  sections[currentSection].style.display = "none";
+
+  // Hide the "Read More" button for the previous section
+  button.style.display = "none";
+
+  currentSection++;
+  if (currentSection < sections.length) {
+    sections[currentSection].style.display = "block";
+
+    // Update the "Read More" button text for the new section
+    if (currentSection < sections.length - 1) {
+      const nextSection = sections[currentSection + 1];
+      button.textContent =
+        "Read More about " +
+        nextSection.querySelector(".bg-green-500").textContent;
+      nextSection.querySelector("button").style.display = "block";
+    }
+  }
+}
+
+// Initialize to display the first section
+sections[currentSection].style.display = "block";
